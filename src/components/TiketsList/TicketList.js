96e7c81ab6d2f29faaa5a1f398372c8312b5b029
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-05-20 17:34:03 +0300

    Added filter logic.

--- src/components/TiketsList/TicketList.js
+++ src/components/TiketsList/TicketList.js
@@ -3,10 +3,10 @@ import Grid from '@material-ui/core/Grid';
 import TicketItem from '../TicketItem';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
-import { getAllTickets } from '../../redux/tickets/tickets-selectors';
+import { getFilteredTickets } from '../../redux/tickets/tickets-selectors';
 
 const TicketsList = () => {
-  const fiveItems = useSelector(getAllTickets).slice(0, 5);
+  const fiveItems = useSelector(getFilteredTickets).slice(0, 5);
 
   return (
     <Grid container component="ul" spacing={2}>
--- src/redux/tickets/tickets-selectors.js
+++ src/redux/tickets/tickets-selectors.js
@@ -1,9 +1,59 @@
 // Imports from Redux Toolkit
-// import { createSelector } from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
+import { getAllFilters } from '../filters/filters-selectors';
+import _ from 'lodash';
 
 // Selector to get all tickets
 const getAllTickets = state => state.tickets.allTickets;
 
 const getTicketsLoading = state => state.tickets.ticketsLoading;
 
-export { getAllTickets, getTicketsLoading };
+// Selector to get filted tickets
+const getFilteredTickets = createSelector(
+  [getAllTickets, getAllFilters],
+  (tickets, filters) => {
+    let filteredTickets = [];
+
+    if (filters.all) {
+      filteredTickets = [...tickets];
+
+      return filteredTickets;
+    }
+
+    if (filters.noTransfer) {
+      const noTransferTickets = tickets.filter(({ segments }) =>
+        segments.every(({ stops }) => _.isEmpty(stops)),
+      );
+
+      filteredTickets = [...filteredTickets, ...noTransferTickets];
+    }
+
+    if (filters.oneTransfer) {
+      const oneTransferTickets = tickets.filter(({ segments }) =>
+        segments.every(({ stops }) => stops.length === 1),
+      );
+
+      filteredTickets = [...filteredTickets, ...oneTransferTickets];
+    }
+
+    if (filters.twoTransfers) {
+      const twoTransfersTickets = tickets.filter(({ segments }) =>
+        segments.every(({ stops }) => stops.length === 2),
+      );
+
+      filteredTickets = [...filteredTickets, ...twoTransfersTickets];
+    }
+
+    if (filters.threeTransfers) {
+      const threeTransfersTickets = tickets.filter(({ segments }) =>
+        segments.every(({ stops }) => stops.length === 3),
+      );
+
+      filteredTickets = [...filteredTickets, ...threeTransfersTickets];
+    }
+
+    return filteredTickets;
+  },
+);
+
+export { getAllTickets, getTicketsLoading, getFilteredTickets };
